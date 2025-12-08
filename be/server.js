152c86d4c25f5abfe35b0ba838e3b702b3223683
Pratik Chaudhary <pratikchaudhary685@gmail.com>
2025-12-07 23:21:21 -0500

    schema created

---
+++ be/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import fileUpload from "express-fileupload";
+import { config } from "dotenv";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  fileUpload({
+    limits: { fileSize: config.maxFileSize },
+    abortOnLimit: true,
+  })
+);
+
+app.use("/api", authRoutes);
+app.use("/api", fileRoutes);
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong!" });
+});
+
+app.listen(config.port, () => {
+  console.log(`Server running on http://localhost:${config.port}`);
+});
